7a7b1d00bb3ad0cc236e66e67a88da5dfeae991b
iridescent <2389504513@qq.com>
2025-01-06 00:07:12 +0800

    ✨ feat: create use-random-color hook

---
+++ libs/ui/hooks/use-random-color/src/lib/use-random-color.tsx
@@ -0,0 +1,9 @@
+export function UseRandomColor() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to UseRandomColor!</h1>
+    </div>
+  );
+}
+
+export default UseRandomColor;
